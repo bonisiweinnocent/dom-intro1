ac4d32df56d36c60437af1696a1421e5ff4cc5aa
bonisiweinnocent <bonisiwe.cukatha@gmail.com>
2021-04-21 10:39:09 +0200

    upate

---
+++ js/calculate-bill.js
@@ -0,0 +1,54 @@
+//get a reference to the calculate button
+var billStringElement = document.querySelector(".billString");
+//get a reference to the billTotal element
+var calculateBtnElement = document.querySelector(".calculateBtn");
+var billTotalElement = document.querySelector(".billTotal")
+// add event listener
+// add event listener
+ var billTotalSpanElement = document.querySelector(".total");
+
+
+
+    // logic goes here
+
+
+
+
+//get a reference to the billString
+
+function calculateBtnClicked(){
+  // get the string entered in the textArea
+  var billString = billStringElement.value;
+  //split the string
+  var billItems = billString.split(",");
+  // a variable for the total phone bill.
+  var billTotal = 0;
+  //loop over all the bill items
+  for (var i=0;i<billItems.length;i++){
+      var billItem = billItems[i].trim();
+      if (billItem === "call"){
+          billTotal += 2.75;
+      }
+      else if (billItem === "sms"){
+          billTotal  += 0.75;
+      }
+  }
+  
+  //round to two decimals
+  var roundedBillTotal = billTotal.toFixed(2);
+  billTotalElement.innerHTML = roundedBillTotal;
+
+//   var currentTotal= Number(roundedlBillTotal);
+  if(roundedBillTotal >=30){
+    billTotalSpanElement.classList.add("danger")
+  
+}else if
+  (roundedBillTotal >=20){
+    billTotalSpanElement.classList.add("warning")
+
+}
+
+}
+calculateBtnElement.addEventListener('click', calculateBtnClicked);
+
+//link the function to a click event on the calculate button
---
+++ js/text-bill.js
@@ -0,0 +1,15 @@
+// get a reference to the textbox where the bill type is to be entered
+//var billStringElement = document.querySelector(".billString");
+var billTypeTextElement = document.querySelector(".billTypeText");
+
+var buttonElement = document.querySelector(".button");
+//get a reference to the add button
+
+//create a variable that will keep track of the total bill
+var billTot = total;
+//add an event listener for when the add button is pressed
+buttonElement.addEventListener('click,')
+//in the event listener check if the value in the bill type textbox is 'sms' or 'call'
+// * add the appropriate value to the running total
+// * add nothing for invalid values that is not 'call' or 'sms'.
+// * display the latest total on the screen
